faa641c385a7ace30cb31f7af81207d470b19033
mila9821 <019101716b@uandina.edu.pe>
2021-11-22 09:55:08 -0800

    HotelZ

--- src/main/java/pe/edu/uandina/demo2Spring/modelo/control/RestauranteController.java
+++ src/main/java/pe/edu/uandina/demo2Spring/modelo/control/RestauranteController.java
@@ -1,6 +1,7 @@
 package pe.edu.uandina.demo2Spring.modelo.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.uandina.demo2Spring.modelo.Restaurante;
@@ -14,6 +15,7 @@ public class RestauranteController {
     @Autowired
     private IRestauranteService restauranteService;
 
+    @GetMapping("/restaurante")
     public List<Restaurante> listar(){
         return restauranteService.findAll();
     }
